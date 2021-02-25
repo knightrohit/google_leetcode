21e12a00a8f8fe894b96c6d5b46e03b895b55d11
Rohit <knight.rohit@gmail.com>
2021-02-25 01:06:01 -0600

    using two pointer

--- backspace_string_compare.py
+++ backspace_string_compare.py
@@ -1,5 +1,5 @@
 """
-Time/Space Complexity = O(N)
+Time/Space Complexity = O(M+N)
 """
 
 class Solution:
@@ -16,3 +16,25 @@ class Solution:
             return stack
         
         return chk(S) == chk(T)
+
+
+"""
+Time Complexity = O(M+N)
+Space Complexity = O(1)
+"""
+from itertools import zip_longest
+
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        
+        def chk(inp):
+            skip = 0
+            for i in reversed(inp):
+                if i == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield i
+        
+        return all(x == y for x, y in zip_longest(chk(S), chk(T)))
